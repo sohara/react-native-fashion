e4b5e951a207daee2ebbe98138a0f488cab4bacb
Sean O'Hara <sohara@sohara.com>
2022-01-16 12:15:21 -0500

    Add tsconfig, eslint, react navigation, etc.

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// interface OnboardingProps {}
+
+export const Onboarding = () => {
+  return <View style={{ flex: 1, backgroundColor: 'cyan' }} />;
+};
