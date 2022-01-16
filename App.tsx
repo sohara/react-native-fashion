e2bab129138bbe22799c175364feef0773a4c221
Sean O'Hara <sohara@sohara.com>
2022-01-16 15:01:50 -0500

    Add fonts and LoadAssets component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Onboarding } from './src/Authentication/Onboarding';
+import { LoadAssets } from './src/components';
+
+const fonts = {
+  'SFProText-Bold': require('./assets/fonts/SF-Pro-Text-Bold.otf'),
+  'SFProText-Semibold': require('./assets/fonts/SF-Pro-Text-Semibold.otf'),
+  'SFProText-Regular': require('./assets/fonts/SF-Pro-Text-Regular.otf'),
+};
 
 const AuthenticationStack = createNativeStackNavigator();
 
 const AuthenticationNavigator = () => {
   return (
-    <AuthenticationStack.Navigator>
+    <AuthenticationStack.Navigator screenOptions={{ headerShown: false }}>
       <AuthenticationStack.Screen name="Onboarding" component={Onboarding} />
     </AuthenticationStack.Navigator>
   );
@@ -16,8 +22,8 @@ const AuthenticationNavigator = () => {
 // eslint-disable-next-line import/no-default-export
 export default function App() {
   return (
-    <NavigationContainer>
+    <LoadAssets fonts={fonts}>
       <AuthenticationNavigator />
-    </NavigationContainer>
+    </LoadAssets>
   );
 }
